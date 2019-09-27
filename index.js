018753d6b8d2c04b74c2bd91f9807b160f4f9102
Leandro Cotrim <cotrim.leandro@gmail.com>
2019-09-27 14:42:21 -0300

    Adicionando funcionalidades no botões

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 load = function () {
     document.getElementById('btnRedirect')
         .addEventListener('click',
-            function () {
+            function () {                
                 console.log('btnRedirect');
+                window.location.href = 'https://www.cielo.com.br';
             });
 
     document.getElementById('btnOpenUrlAndClose')
         .addEventListener('click',
             function () {
                 console.log('btnOpenUrlAndClose');
+                window.open('https://www.cielo.com.br');
+                window.close();
             });
 }
